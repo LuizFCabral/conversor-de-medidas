a70c05e67f9716ea6c52108c6515e529c13ba3ae
Luiz Cabral <luizfelipe.cabralsouza@gmail.com>
2023-04-03 13:10:50 -0300

    :sparkles: feat: Adiciona componentes básicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ConversorComponent } from './conversor/conversor.component';
+import { InputComponent } from './input/input.component';
+import { SelectComponent } from './select/select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConversorComponent,
+    InputComponent,
+    SelectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
